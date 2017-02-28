3c1fbad5d323555e72872ee5c25750628ba77da1
optiminer <optiminer@tutamail.com>
2017-02-28 23:57:30 +0100

    Fork Zero optiminer release repo.

--- make_release.sh
+++ make_release.sh
@@ -6,22 +6,22 @@ then
   VERSION="noname"
 fi
 
-rm -R optiminer-zcash/bin optiminer-zcash/logs
-mkdir -p optiminer-zcash/bin
-cp -r ~/programme/optiminer-zcash/bin/*.bin optiminer-zcash/bin/
+rm -R optiminer-zero/bin optiminer-zero/logs
+mkdir -p optiminer-zero/bin
+cp -r ~/programme/optiminer-zcash/bin/*.bin optiminer-zero/bin/
 
-for V in 191205 200406 203603 207903 223600
-do
-  mkdir -p "optiminer-zcash/bin-$V"
-  cp -r ~/programme/optiminer-zcash/bin-$V/*.bin "optiminer-zcash/bin-$V/"
-done
+#for V in 191205 200406 203603 207903 223600
+#do
+#B  mkdir -p "optiminer-zero/bin-$V"
+#  cp -r ~/programme/optiminer-zero/bin-$V/*.bin "optiminer-zero/bin-$V/"
+#done
 
-cp ~/programme/optiminer-zcash/build/optiminer-zcash optiminer-zcash/
-cp ~/programme/optiminer-zcash/LICENSE optiminer-zcash/
-cp ~/programme/optiminer-zcash/openssl-license.txt optiminer-zcash/
-cp ~/programme/optiminer-zcash/mine.sh optiminer-zcash/
-cp ~/programme/optiminer-zcash/watchdog-cmd.sh optiminer-zcash/
-cp README.md optiminer-zcash/
-strip optiminer-zcash/optiminer-zcash
-tar -h -czvf optiminer-zcash-${VERSION}.tar.gz optiminer-zcash/*
+cp ~/programme/optiminer-zcash/build/optiminer-zero optiminer-zero/
+cp ~/programme/optiminer-zcash/LICENSE optiminer-zero/
+cp ~/programme/optiminer-zcash/openssl-license.txt optiminer-zero/
+cp ~/programme/optiminer-zcash/mine.sh optiminer-zero/
+cp ~/programme/optiminer-zcash/watchdog-cmd.sh optiminer-zero/
+cp README.md optiminer-zero/
+strip optiminer-zero/optiminer-zero
+tar -h -czvf optiminer-zero-${VERSION}.tar.gz optiminer-zero/*
 
